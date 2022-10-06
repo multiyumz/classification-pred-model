2fcdfb0d2ee6913f35badbc1c09b7763542511d7
Thomas Johnson <johnsonthomas467@gmail.com>
2022-10-06 15:46:55 +0100

    app file completed

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from keras.applications.vgg16 import VGG16
 
 app = Flask(__name__)
 
+model = VGG16
+
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html')
@@ -18,7 +20,17 @@ def predict():
     image_path = "./images/" + image_file.filename
     image_file.save(image_path)
 
-    return render_template('index.html')
+    image = load_img(image_path, target_size=(244,244))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(yhat)
+    label = decode_predictions(yhat)
+    label = label[0][0]
+
+    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
+
+    return render_template('index.html', prediction=classification)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
